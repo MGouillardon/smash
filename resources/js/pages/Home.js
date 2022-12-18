2260b6d5fb22d2db442e8811d1a5a3009cf0b77a
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2022-12-18 20:07:30 +0100

    feat: add fetch api

--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -5,31 +5,9 @@ export default {
             WHICH CHAMP DO U SMASH
         </h1>
         <div class="h-3/4 flex flex-col items-center justify-center space-y-6 md:space-y-20 lg:flex-row lg:space-y-0 lg:w-3/4 lg:justify-evenly">
-            <div class="flex flex-col">
-                <h2 class="text-5xl text-center md:text-7xl">NAME</h2>
-                <div class="w-32 h-32 flex justify-evenly items-center">
-                    <a href="#">
-                        <img
-                            class=""
-                            src="http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/Aatrox.png"
-                            alt=""
-                        />
-                    </a>
-                </div>
-            </div>
+            <div class="flex flex-col" id="first-champion-js"></div>
             <p class="text-5xl md:text-7xl lg:pt-20">OR</p>
-            <div class="flex flex-col-reverse lg:flex-col">
-                <h2 class="text-5xl text-center md:text-7xl pt-2 lg:pt-0">NAME</h2>
-                <div class="w-32 h-32 flex justify-center items-center">
-                    <a href="#">
-                        <img
-                            class=""
-                            src="http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/Aatrox.png"
-                            alt=""
-                        />
-                    </a>
-                </div>
-            </div>
+            <div class="flex flex-col-reverse lg:flex-col" id="second-champion-js"></div>
         </div>
     
         <a
@@ -40,5 +18,44 @@ export default {
         </a>
     </div>;
     `,
-    after_render: async () => {},
+    after_render: async () => {
+        const DUEL = '/api/duel';
+        async function fetchToJSON(URL) {
+            const response = await fetch(URL);
+            const data = await response.json();
+            return data;
+        }
+        const DATA = await fetchToJSON(DUEL);
+        const FIRST_CHAMP = document.getElementById('first-champion-js');
+        const IMG_NAME_ONE = DATA[0]
+            .replace("'", '')
+            .replace(' ', '')
+            .replace('&');
+        const IMG_NAME_TWO = DATA[1].replace("'", '');
+        FIRST_CHAMP.innerHTML += `
+    <h2 class="text-5xl text-center md:text-7xl">${DATA[0]}</h2>
+                <div class="w-32 h-32 flex justify-evenly items-center">
+                    <a href="#">
+                        <img
+                            class=""
+                            src="http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/${IMG_NAME_ONE}.png"
+                            alt="${DATA[0]}"
+                        />
+                    </a>
+                </div>
+    `;
+        const SECOND_CHAMP = document.getElementById('second-champion-js');
+        SECOND_CHAMP.innerHTML += `
+    <h2 class="text-5xl text-center md:text-7xl pt-2 lg:pt-0">${DATA[1]}</h2>
+                <div class="w-32 h-32 flex justify-center items-center">
+                    <a href="#">
+                        <img
+                            class=""
+                            src="http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/${IMG_NAME_TWO}.png"
+                            alt="${DATA[1]}"
+                        />
+                    </a>
+                </div>
+    `;
+    },
 };
